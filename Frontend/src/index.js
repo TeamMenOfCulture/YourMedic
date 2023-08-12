a580a68f2d0b63f13f140b52aa608ec535ceabab
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-08-12 13:16:41 +0530

    OpenAI Addded

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {Auth0Provider} from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="doctorgpt.us.auth0.com"
+      clientId="hehsIrOMgX8mwSfTObnmyDlZP2GM1Ohb"
+      redirecturi={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
