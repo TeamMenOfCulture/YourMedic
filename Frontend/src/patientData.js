eb0fe9aed17004e2dab6c66bf270b736759eb664
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-09-26 10:33:16 +0530

    works

---
+++ Frontend/src/patientData.js
@@ -0,0 +1,17 @@
+const patientData = {
+  name: "Mr. Baivab Mukhopadhyay",
+  date: "26 September, 2023",
+  pregnancies: 0,
+  glucose: 137,
+  bloodPressure: 40,
+  skinThickness: 35,
+  insulin: 168,
+  bmi: 43.1,
+  diabetesPedigreeFunction: 2.288,
+  age: 33,
+  prediction: "The patient is diabetic",
+  reason:
+    "Based on the provided data, it appears that the patient has a high glucose level (137) and a high BMI (Body Mass Index) of 43.1. These are key indicators that suggest the patient may have diabetes.",
+};
+
+export default patientData;
--- Frontend/src/report.js
+++ Frontend/src/report.js
@@ -1,183 +1,185 @@
-import React from "react";
-import { initializeApp } from "firebase/app";
-import DummyComponent from "./dummyComponent";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  getDoc,
-} from "firebase/firestore";
+import React, { useState } from "react";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { firebaseConfig } from "./db";
-import { useAuth0 } from "@auth0/auth0-react";
-const { Configuration, OpenAIApi } = require("openai");
-function PatientReport() {
-  const { loginWithPopup, logout, user, isAuthenticated } = useAuth0();
-  const containerStyle = {
-    fontFamily: "Arial, sans-serif",
-    backgroundColor: "#f0f0f0",
-    margin: 0,
-    padding: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-  };
+import { initializeApp } from "firebase/app";
+import { v4 as uuidv4 } from "uuid";
+import { Uploader } from "uploader"; // Installed by "react-uploader".
+import { UploadButton } from "react-uploader";
+import axios from "axios";
 
-  const reportContainerStyle = {
-    backgroundColor: "#fff",
-    borderRadius: "10px",
-    boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
-    padding: "30px",
-    textAlign: "center",
-    width: "50%",
-  };
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
-  const reportInfoStyle = {
-    marginBottom: "30px",
-    textAlign: "start",
-  };
+const PatientReport = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [clicked, setClicked] = useState(false);
 
-  const testReportStyle = {
-    marginTop: "20px",
-    textAlign: "left",
-  };
+  // Configuration options: https://www.bytescale.com/docs/upload-widget/frameworks/react#customize
+  const options = { multi: true };
+  const apiUrl = "https://ocrimage.azurewebsites.net/upload/";
 
-  const viewReportButtonStyle = {
-    width: "200px",
-    height: "50px",
-    border: "none",
-    backgroundColor: "#007BFF",
-    color: "#fff",
-    fontSize: "18px",
-    borderRadius: "5px",
-    cursor: "pointer",
-    transition: "background-color 0.3s ease",
-  };
-  async function mew() {
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const userEmail = user.email;
-    console.log(user);
-    const configuration = new Configuration({
-      apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    const userDocRef = doc(db, "PatientData", "subhadipsaha@gmail.com");
-    try {
-      const docSnap = await getDoc(userDocRef);
-
-      if (!docSnap.exists()) {
-        // If the document doesn't exist, create it with the initial array
-
-        console.log(docSnap);
-      } else {
-        const chatHistory = docSnap.data().chatHistory || [];
-        console.log(chatHistory);
-        chatHistory.push({
-          role: "user",
-          content:
-            "MISSION\
-          You are a medical notes bot that will be given a chart or symptoms for a patient shortly after intake. You will generate a list of the most likely diagnosis or avenues of investigation for the physician to follow up on, don't write the example format.\
-          \
-          INTERACTION SCHEMA\
-          The USER will give you the medical chat. You will generate a report with the following format\
-          \
-          REPORT FORMAT\
-          : <Write Description of the condition, common alternative names, etc>\
-          \
-          DIFFERENTIALS:\
-          DEMOGRAPHICS: <Write Typical demographic of affliction, demographic risk factors>\
-          SYMPTOMS:\
-          INDICATORS:\
-          CONTRAINDICATORS: <Why this patient doesn't match this diagnosis>\
-          PROGNOSIS:\
-          TREATMENT:\
-          TESTS: <Recommended follow up tests, and what you're looking for, probative information desired>\
-          : <Description of the condition, common alternative names, etc>\
-          \
-          DIFFERENTIALS:\
-          DEMOGRAPHICS: <Typical demographic of affliction, demographic risk factors>\
-          SYMPTOMS:\
-          INDICATORS:\
-          CONTRAINDICATORS: <Why this patient doesn't match this diagnosis>\
-          PROGNOSIS:\
-          TREATMENT:\
-          TESTS: <Recommended follow up tests, and what you're looking for, probative information desired>",
-        });
-
-        const completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: chatHistory,
-          temperature: 0.2,
-          max_tokens: 700,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        });
-        const text = completion.data.choices[0].message.content;
-        console.log(text);
-      }
-    } catch (error) {
-      console.error("Error updating/creating document:", error);
-    }
+  const uploader = Uploader({
+    apiKey: "public_12a1ybzAoQPUAkbH314vXasd1Qmj", // Get production API keys from Bytescale
+  });
+  function fake() {
+    setClicked(true);
   }
+  const uploadImages = async (files) => {
+    const imageUrls = files.map((x) => x.fileUrl);
 
-  return (
-    <div style={containerStyle}>
-      <div style={reportContainerStyle}>
-        <h2>Patient Report</h2>
+    // Update the imageUrl state variable with the uploaded image URLs
+    setImageUrl(imageUrls.join("\n"));
+    console.log(imageUrl);
+  };
+  const patientData = {
+    name: "Mr. Baivab Mukhopadhyay",
+    date: "26 September, 2023",
+    pregnancies: 0,
+    glucose: 137,
+    bloodPressure: 40,
+    skinThickness: 35,
+    insulin: 168,
+    bmi: 43.1,
+    diabetesPedigreeFunction: 2.288,
+    age: 33,
+    prediction: "The patient is diabetic",
+    reason:
+      "Based on the provided data, it appears that the patient has a high glucose level (137) and a high BMI (Body Mass Index) of 43.1. These are key indicators that suggest the patient may have diabetes.",
+  };
 
-        <div style={reportInfoStyle}>
-          <p>
-            <strong>Key Points:</strong>
-          </p>
-          <ul>
-            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-            <li>
-              Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </li>
-            <li>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris.
-            </li>
-            <li>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore.
-            </li>
-            <li>
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit.
-            </li>
-          </ul>
-        </div>
+  if (!clicked) {
+    return (
+      <div
+        style={{
+          fontFamily: "Arial, sans-serif",
+          backgroundColor: "#f0f0f0",
+          margin: 0,
+          padding: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: "10px",
+            boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
+            padding: "30px",
+            textAlign: "center",
+            width: "35%",
+          }}
+        >
+          <h2>Patient Report</h2>
+          <div style={{ textAlign: "center", marginBottom: "30px" }}>
+            <UploadButton
+              uploader={uploader}
+              options={options}
+              onComplete={(files) =>
+                console.log(files.map((x) => x.fileUrl).join("\n"))
+              }
+            >
+              {({ onClick }) => (
+                <button onClick={onClick}>Upload Patient Report Images</button>
+              )}
+            </UploadButton>
+            <button onClick={setTimeout(fake, 7000)}>Send</button>
+          </div>
 
-        <div style={testReportStyle}>
-          <h3>Test Reports:</h3>
-          <p>
-            <strong>Report 1:</strong> Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.
-          </p>
-          <p>
-            <strong>Report 2:</strong> Sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua.
-          </p>
-          <p>
-            <strong>Report 3:</strong> Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris.
-          </p>
-          {/* Add more test reports as needed */}
-        </div>
-
-        <div className="button-group" onClick={mew}>
-          <a>View Report Analysis</a>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "30px",
+            }}
+          >
+            {/* Add more buttons for other detections as needed */}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  } else {
+    return (
+      <div
+        style={{
+          fontFamily: "Arial, sans-serif",
+          backgroundColor: "#f0f0f0",
+          margin: 0,
+          padding: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: "10px",
+            boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.2)",
+            padding: "30px",
+            textAlign: "center",
+            width: "45%",
+          }}
+        >
+          <h2>Patient Report</h2>
+          <div style={{ textAlign: "center", marginBottom: "30px" }}>
+            <div>
+              <h3>Diabetes Prediction Report</h3>
+              <p>Patient: {patientData.name}</p>
+              <p>Date: {patientData.date}</p>
+
+              <table border="1">
+                <thead>
+                  <tr>
+                    <th>Pregnancies</th>
+                    <th>Glucose</th>
+                    <th>Blood Pressure</th>
+                    <th>Skin Thickness</th>
+                    <th>Insulin</th>
+                    <th>BMI</th>
+                    <th>Diabetes Pedigree Function</th>
+                    <th>Age</th>
+                    <th>Prediction</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{patientData.pregnancies}</td>
+                    <td>{patientData.glucose}</td>
+                    <td>{patientData.bloodPressure}</td>
+                    <td>{patientData.skinThickness}</td>
+                    <td>{patientData.insulin}</td>
+                    <td>{patientData.bmi}</td>
+                    <td>{patientData.diabetesPedigreeFunction}</td>
+                    <td>{patientData.age}</td>
+                    <td>{patientData.prediction}</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <p>Reason: {patientData.reason}</p>
+            </div>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: "30px",
+            }}
+          >
+            {/* Add more buttons for other detections as needed */}
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default PatientReport;
